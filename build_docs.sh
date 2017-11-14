f58def3ed093ea9b170a961036c33567e8c4e775
Pete Keen <pete@petekeen.net>
2017-11-13 21:58:06 -0500

    bump

--- build_docs.sh
+++ build_docs.sh
@@ -7,6 +7,8 @@ mkdir -p _site/3.0/doc
 curl https://raw.githubusercontent.com/ledger/ledger/v3.1.1/doc/ledger3.texi > ledger3.texi
 curl https://raw.githubusercontent.com/ledger/ledger/v3.1.1/doc/version.texi.in > version.texi
 curl https://raw.githubusercontent.com/ledger/ledger/v3.1.1/doc/ledger.1 > ledger.1
+curl https://raw.githubusercontent.com/ledger/ledger-mode/master/doc/ledger-mode.texi > ledger-mode.texi
 
 makeinfo --force --html --no-split -o _site/3.0/doc/ledger3.html ledger3.texi
+makeinfo --force --html --no-split -o _site/3.0/doc/ledger-mode.html ledger-mode.texi
 groff -mandoc -Thtml ledger.1 > _site/3.0/doc/ledger.1.html
