49f95d78f8fca69acbc2493c08e9643c103744b5
Pete Keen <pete@petekeen.net>
2017-11-14 09:41:20 -0500

    Verbose texi2pdf output

--- build_docs.sh
+++ build_docs.sh
@@ -12,5 +12,5 @@ curl https://raw.githubusercontent.com/ledger/ledger-mode/master/doc/ledger-mode
 makeinfo --force --html --no-split -o _site/3.0/doc/ledger3.html ledger3.texi
 makeinfo --force --html --no-split -o _site/3.0/doc/ledger-mode.html ledger-mode.texi
 
-texi2pdf -b -q -o _site/3.0/doc/ledger3.pdf ledger3.texi
+texi2pdf -b -V -o _site/3.0/doc/ledger3.pdf ledger3.texi
 groff -mandoc -Thtml ledger.1 > _site/3.0/doc/ledger.1.html
